c6c7f6876a4847ed0560a8827550ec031606bb0f
Evel25 <bi3.h300@gmail.com>
2020-02-21 18:32:04 +0100

    adding the recycler adapter and fetch data from the database

--- app/src/main/java/agh/softdev/taskmanager/MainActivity.java
+++ app/src/main/java/agh/softdev/taskmanager/MainActivity.java
@@ -4,29 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import java.util.List;
 
+import agh.softdev.taskmanager.adapter.TasksAdapter;
 import agh.softdev.taskmanager.database.entities.Task;
 import agh.softdev.taskmanager.viewmodel.TaskViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private TaskViewModel taskViewModel;
+    private RecyclerView tasksRecycler;
+    private TasksAdapter tasksAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tasksRecycler = findViewById(R.id.taskItems);
+        tasksAdapter = new TasksAdapter();
+        tasksRecycler.setLayoutManager(new LinearLayoutManager(this));
+        tasksRecycler.setAdapter(tasksAdapter);
 
         taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
         // getting data from the livedata object
         taskViewModel.getAllTasks().observe(this, new Observer<List<Task>>() {
             @Override
             public void onChanged(List<Task> tasks) {
-                Toast.makeText(MainActivity.this, "The Database Is Created And Data Has Been Changer", Toast.LENGTH_SHORT).show();
+                tasksAdapter.setTasks(tasks);
             }
         });
 
---
+++ app/src/main/java/agh/softdev/taskmanager/adapter/TasksAdapter.java
@@ -0,0 +1,54 @@
+package agh.softdev.taskmanager.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import agh.softdev.taskmanager.R;
+import agh.softdev.taskmanager.database.entities.Task;
+
+public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TaskHolder> {
+
+    private List<Task> tasks = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item =  LayoutInflater.from(parent.getContext()).inflate(R.layout.task_recycler_item,parent,false);
+        return new TaskHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+        holder.taskName.setText(tasks.get(position).getTitle());
+        holder.taskDesc.setText(tasks.get(position).getDesctiprion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    public void setTasks(List<Task> tasks){
+        this.tasks = tasks;
+        this.notifyDataSetChanged();
+    }
+
+    class TaskHolder extends RecyclerView.ViewHolder{
+
+        private TextView taskName,taskDesc;
+
+        private TaskHolder(@NonNull View itemView) {
+            super(itemView);
+            this.taskName = itemView.findViewById(R.id.taskName);
+            this.taskDesc = itemView.findViewById(R.id.taskDesc);
+        }
+    }
+}
