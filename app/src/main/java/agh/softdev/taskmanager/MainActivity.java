65a58e5239e83551dfe0b53a0d01bdabad62a256
Evel25 <bi3.h300@gmail.com>
2020-02-21 15:30:43 +0100

    Adding our view Model And Getting An Instance Of it in our main activity

--- app/src/main/java/agh/softdev/taskmanager/MainActivity.java
+++ app/src/main/java/agh/softdev/taskmanager/MainActivity.java
@@ -1,14 +1,33 @@
 package agh.softdev.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import agh.softdev.taskmanager.database.entities.Task;
+import agh.softdev.taskmanager.viewmodel.TaskViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TaskViewModel taskViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
+        // getting data from the livedata object
+        taskViewModel.getAllTasks().observe(this, new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                // set the data to the activity
+                Toast.makeText(MainActivity.this, "Data has been changed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/agh/softdev/taskmanager/viewmodel/TaskViewModel.java
@@ -0,0 +1,41 @@
+package agh.softdev.taskmanager.viewmodel;
+
+import android.app.Application;
+import android.text.method.MultiTapKeyListener;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import agh.softdev.taskmanager.database.entities.Task;
+import agh.softdev.taskmanager.repository.TaskRepo;
+
+public class TaskViewModel extends AndroidViewModel {
+
+    private TaskRepo repository;
+    private LiveData<List<Task>> allTasks;
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+        repository = new TaskRepo(application);
+        allTasks = repository.getAllTasks();
+    }
+
+    public void insert(Task task){
+        repository.insert(task);
+    }
+
+    public void delete(Task task){
+        repository.delete(task);
+    }
+
+    public void update(Task task){
+        repository.update(task);
+    }
+
+    public LiveData<List<Task>> getAllTasks(){
+        return allTasks;
+    }
+}
